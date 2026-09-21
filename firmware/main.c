53e420286b5cc2f745c44f1d644cc0bbd7998408
Hiroki Mori <yamori813@yahoo.co.jp>
2026-09-21 10:44:51 +0900

    first commit

---
+++ firmware/main.c
@@ -0,0 +1,330 @@
+/* Name: main.c
+ * Project: Thermostat based on AVR USB driver
+ * Author: Christian Starkjohann
+ * Creation Date: 2006-04-23
+ * Tabsize: 4
+ * Copyright: (c) 2006 by OBJECTIVE DEVELOPMENT Software GmbH
+ * License: Proprietary, free under certain conditions. See Documentation.
+ * This Revision: $Id: main.c 537 2008-02-28 21:13:01Z cs $
+ */
+
+#include <avr/io.h>
+#include <avr/wdt.h>
+#include <avr/eeprom.h>
+#include <avr/interrupt.h>
+#include <avr/pgmspace.h>
+#include <util/delay.h>
+
+#include "usbdrv.h"
+#include "oddebug.h"
+
+/*
+Pin assignment:
+PB1 = key input (active low with pull-up)
+
+PB0, PB2 = USB data lines
+*/
+
+#define BIT_KEY 1
+
+#define REVERS_KEY
+
+#define UTIL_BIN4(x)        (uchar)((0##x & 01000)/64 + (0##x & 0100)/16 + (0##x & 010)/4 + (0##x & 1))
+#define UTIL_BIN8(hi, lo)   (uchar)(UTIL_BIN4(hi) * 16 + UTIL_BIN4(lo))
+
+#ifndef NULL
+#define NULL    ((void *)0)
+#endif
+
+/* ------------------------------------------------------------------------- */
+
+static uchar    reportBuffer[2];    /* buffer for HID reports */
+static uchar    idleRate;           /* in 4 ms units */
+
+/* ------------------------------------------------------------------------- */
+PROGMEM const char usbHidReportDescriptor[34] = {
+    0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
+    0x09, 0x05,                    // USAGE (Game Pad)
+    0xa1, 0x01,                    // COLLECTION (Application)
+    0x09, 0x05,                    //   USAGE (Game Pad)
+    0xa1, 0x00,                    //   COLLECTION (Physical)
+    0x05, 0x09,                    //     USAGE_PAGE (Button)
+    0x19, 0x01,                    //     USAGE_MINIMUM (Button 1)
+    0x29, 0x01,                    //     USAGE_MAXIMUM (Button 1)
+    0x15, 0x00,                    //     LOGICAL_MINIMUM (0)
+    0x25, 0x01,                    //     LOGICAL_MAXIMUM (1)
+    0x95, 0x01,                    //     REPORT_COUNT (1)
+    0x75, 0x01,                    //     REPORT_SIZE (1)
+    0x81, 0x02,                    //     INPUT (Data,Var,Abs)
+    0x95, 0x01,                    //     REPORT_COUNT (1)
+    0x75, 0x07,                    //     REPORT_SIZE (7)
+    0x81, 0x03,                    //     INPUT (Cnst,Var,Abs)
+    0xc0,                          //     END_COLLECTION
+    0xc0                           // END_COLLECTION
+};
+
+/* We use a simplifed keyboard report descriptor which does not support the
+ * boot protocol. We don't allow setting status LEDs and we only allow one
+ * simultaneous key press (except modifiers). We can therefore use short
+ * 2 byte input reports.
+ * The report descriptor has been created with usb.org's "HID Descriptor Tool"
+ * which can be downloaded from http://www.usb.org/developers/hidpage/.
+ * Redundant entries (such as LOGICAL_MINIMUM and USAGE_PAGE) have been omitted
+ * for the second INPUT item.
+ */
+
+/* Keyboard usage values, see usb.org's HID-usage-tables document, chapter
+ * 10 Keyboard/Keypad Page for more codes.
+ */
+#define MOD_CONTROL_LEFT    (1<<0)
+#define MOD_SHIFT_LEFT      (1<<1)
+#define MOD_ALT_LEFT        (1<<2)
+#define MOD_GUI_LEFT        (1<<3)
+#define MOD_CONTROL_RIGHT   (1<<4)
+#define MOD_SHIFT_RIGHT     (1<<5)
+#define MOD_ALT_RIGHT       (1<<6)
+#define MOD_GUI_RIGHT       (1<<7)
+
+#define KEY_A       4
+#define KEY_B       5
+#define KEY_C       6
+#define KEY_D       7
+#define KEY_E       8
+#define KEY_F       9
+#define KEY_G       10
+#define KEY_H       11
+#define KEY_I       12
+#define KEY_J       13
+#define KEY_K       14
+#define KEY_L       15
+#define KEY_M       16
+#define KEY_N       17
+#define KEY_O       18
+#define KEY_P       19
+#define KEY_Q       20
+#define KEY_R       21
+#define KEY_S       22
+#define KEY_T       23
+#define KEY_U       24
+#define KEY_V       25
+#define KEY_W       26
+#define KEY_X       27
+#define KEY_Y       28
+#define KEY_Z       29
+#define KEY_1       30
+#define KEY_2       31
+#define KEY_3       32
+#define KEY_4       33
+#define KEY_5       34
+#define KEY_6       35
+#define KEY_7       36
+#define KEY_8       37
+#define KEY_9       38
+#define KEY_0       39
+
+#define KEY_F1      58
+#define KEY_F2      59
+#define KEY_F3      60
+#define KEY_F4      61
+#define KEY_F5      62
+#define KEY_F6      63
+#define KEY_F7      64
+#define KEY_F8      65
+#define KEY_F9      66
+#define KEY_F10     67
+#define KEY_F11     68
+#define KEY_F12     69
+
+#define KEY_Space   0x2c
+
+/* ------------------------------------------------------------------------- */
+
+#define NUM_KEYS    5
+
+static const uchar  keyReport[NUM_KEYS + 1][2] PROGMEM = {
+	/* none */  {0, 0},                     /* no key pressed */
+	/*  1 */    // {0, KEY_Space},
+	/*  1 */    {MOD_GUI_LEFT, KEY_L},
+	/*  2 */    {MOD_SHIFT_LEFT, KEY_2},
+	/*  3 */    {MOD_SHIFT_LEFT, KEY_3},
+	/*  4 */    {MOD_SHIFT_LEFT, KEY_4},
+};
+
+static void buildReport(uchar key)
+{
+	/* This (not so elegant) cast saves us 10 bytes of program memory */
+    *(int *)reportBuffer = pgm_read_word(keyReport[key]);
+}
+
+/* ------------------------------------------------------------------------- */
+
+static uchar    keyPressed(void)
+{
+	uchar   mask;
+	
+#ifdef REVERS_KEY
+    if((PINB & (1 << BIT_KEY)) != 0)
+#else
+	if((PINB & (1 << BIT_KEY)) == 0)
+#endif
+		return 1;
+    return 0;
+}
+
+/* ------------------------------------------------------------------------- */
+
+static void timerInit(void)
+{
+    TCCR1 = 0x0b;           /* select clock: 16.5M/1k -> overflow rate = 16.5M/256k = 62.94 Hz */
+}
+
+/* ------------------------------------------------------------------------- */
+/* ------------------------ interface to USB driver ------------------------ */
+/* ------------------------------------------------------------------------- */
+
+uchar	usbFunctionSetup(uchar data[8])
+{
+usbRequest_t    *rq = (void *)data;
+
+    usbMsgPtr = reportBuffer;
+    if((rq->bmRequestType & USBRQ_TYPE_MASK) == USBRQ_TYPE_CLASS){    /* class request type */
+        if(rq->bRequest == USBRQ_HID_GET_REPORT){  /* wValue: ReportType (highbyte), ReportID (lowbyte) */
+            /* we only have one report type, so don't look at wValue */
+            buildReport(keyPressed());
+            return sizeof(reportBuffer);
+        }else if(rq->bRequest == USBRQ_HID_GET_IDLE){
+            usbMsgPtr = &idleRate;
+            return 1;
+        }else if(rq->bRequest == USBRQ_HID_SET_IDLE){
+            idleRate = rq->wValue.bytes[1];
+        }
+    }else{
+        /* no vendor specific requests implemented */
+    }
+	return 0;
+}
+
+/* ------------------------------------------------------------------------- */
+/* ------------------------ Oscillator Calibration ------------------------- */
+/* ------------------------------------------------------------------------- */
+
+/* Calibrate the RC oscillator to 8.25 MHz. The core clock of 16.5 MHz is
+ * derived from the 66 MHz peripheral clock by dividing. Our timing reference
+ * is the Start Of Frame signal (a single SE0 bit) available immediately after
+ * a USB RESET. We first do a binary search for the OSCCAL value and then
+ * optimize this value with a neighboorhod search.
+ * This algorithm may also be used to calibrate the RC oscillator directly to
+ * 12 MHz (no PLL involved, can therefore be used on almost ALL AVRs), but this
+ * is wide outside the spec for the OSCCAL value and the required precision for
+ * the 12 MHz clock! Use the RC oscillator calibrated to 12 MHz for
+ * experimental purposes only!
+ */
+static void calibrateOscillator(void)
+{
+uchar       step = 128;
+uchar       trialValue = 0, optimumValue;
+int         x, optimumDev, targetValue = (unsigned)(1499 * (double)F_CPU / 10.5e6 + 0.5);
+
+    /* do a binary search: */
+    do{
+        OSCCAL = trialValue + step;
+        x = usbMeasureFrameLength();    /* proportional to current real frequency */
+        if(x < targetValue)             /* frequency still too low */
+            trialValue += step;
+        step >>= 1;
+    }while(step > 0);
+    /* We have a precision of +/- 1 for optimum OSCCAL here */
+    /* now do a neighborhood search for optimum value */
+    optimumValue = trialValue;
+    optimumDev = x; /* this is certainly far away from optimum */
+    for(OSCCAL = trialValue - 1; OSCCAL <= trialValue + 1; OSCCAL++){
+        x = usbMeasureFrameLength() - targetValue;
+        if(x < 0)
+            x = -x;
+        if(x < optimumDev){
+            optimumDev = x;
+            optimumValue = OSCCAL;
+        }
+    }
+    OSCCAL = optimumValue;
+}
+/*
+Note: This calibration algorithm may try OSCCAL values of up to 192 even if
+the optimum value is far below 192. It may therefore exceed the allowed clock
+frequency of the CPU in low voltage designs!
+You may replace this search algorithm with any other algorithm you like if
+you have additional constraints such as a maximum CPU clock.
+For version 5.x RC oscillators (those with a split range of 2x128 steps, e.g.
+ATTiny25, ATTiny45, ATTiny85), it may be useful to search for the optimum in
+both regions.
+*/
+
+void    usbEventResetReady(void)
+{
+    calibrateOscillator();
+    eeprom_write_byte(0, OSCCAL);   /* store the calibrated value in EEPROM */
+}
+
+/* ------------------------------------------------------------------------- */
+/* --------------------------------- main ---------------------------------- */
+/* ------------------------------------------------------------------------- */
+
+int main(void)
+{
+uchar   i;
+uchar   calibrationValue;
+uchar   idleCounter = 0;
+uchar   key, lastKey = 0, keyDidChange = 0;
+
+	// this is work around at after wdt reset
+    wdt_enable(WDTO_4S);
+    calibrationValue = eeprom_read_byte(0); /* calibration value from last time */
+    if(calibrationValue != 0xff){
+        OSCCAL = calibrationValue;
+    }
+    odDebugInit();
+    usbDeviceDisconnect();
+    for(i=0;i<20;i++){  /* 300 ms disconnect */
+        _delay_ms(15);
+    }
+    usbDeviceConnect();
+    PORTB |= 1 << BIT_KEY;  /* pull-up on key input */
+    wdt_enable(WDTO_1S);
+    timerInit();
+    usbInit();
+    sei();
+    for(;;){    /* main event loop */
+        wdt_reset();
+        usbPoll();
+		key = keyPressed();
+        if(lastKey != key){
+            lastKey = key;
+            keyDidChange = 1;
+        }
+#if 0
+		if(TIFR & (1<<TOV0)){   /* 22 ms timer */
+            TIFR = 1<<TOV0;
+            if(idleRate != 0){
+                if(idleCounter > 4){
+                    idleCounter -= 5;   /* 22 ms in units of 4 ms */
+                }else{
+                    idleCounter = idleRate;
+                    keyDidChange = 1;
+                }
+            }
+        }
+#endif
+        if(keyDidChange && usbInterruptIsReady()){
+            keyDidChange = 0;
+            /* use last key and not current key status in order to avoid lost
+			 changes in key status. */
+/*
+            buildReport(lastKey);
+            usbSetInterrupt(reportBuffer, sizeof(reportBuffer));
+*/
+            reportBuffer[0] = lastKey;
+            usbSetInterrupt(reportBuffer, 1);
+        }
+    }
+    return 0;
+}
