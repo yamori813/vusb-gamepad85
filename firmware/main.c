e898b447189d3a4fc7c24cca314994d699d1a606
Hiroki Mori <yamori813@yahoo.co.jp>
2026-09-24 07:02:23 +0900

    cleanup

--- firmware/main.c
+++ firmware/main.c
@@ -38,7 +38,7 @@ PB0, PB2 = USB data lines
 
 /* ------------------------------------------------------------------------- */
 
-static uchar    reportBuffer[2];    /* buffer for HID reports */
+static uchar    reportBuffer[1];    /* buffer for HID reports */
 static uchar    idleRate;           /* in 4 ms units */
 
 /* ------------------------------------------------------------------------- */
@@ -63,98 +63,6 @@ PROGMEM const char usbHidReportDescriptor[34] = {
     0xc0                           // END_COLLECTION
 };
 
-/* We use a simplifed keyboard report descriptor which does not support the
- * boot protocol. We don't allow setting status LEDs and we only allow one
- * simultaneous key press (except modifiers). We can therefore use short
- * 2 byte input reports.
- * The report descriptor has been created with usb.org's "HID Descriptor Tool"
- * which can be downloaded from http://www.usb.org/developers/hidpage/.
- * Redundant entries (such as LOGICAL_MINIMUM and USAGE_PAGE) have been omitted
- * for the second INPUT item.
- */
-
-/* Keyboard usage values, see usb.org's HID-usage-tables document, chapter
- * 10 Keyboard/Keypad Page for more codes.
- */
-#define MOD_CONTROL_LEFT    (1<<0)
-#define MOD_SHIFT_LEFT      (1<<1)
-#define MOD_ALT_LEFT        (1<<2)
-#define MOD_GUI_LEFT        (1<<3)
-#define MOD_CONTROL_RIGHT   (1<<4)
-#define MOD_SHIFT_RIGHT     (1<<5)
-#define MOD_ALT_RIGHT       (1<<6)
-#define MOD_GUI_RIGHT       (1<<7)
-
-#define KEY_A       4
-#define KEY_B       5
-#define KEY_C       6
-#define KEY_D       7
-#define KEY_E       8
-#define KEY_F       9
-#define KEY_G       10
-#define KEY_H       11
-#define KEY_I       12
-#define KEY_J       13
-#define KEY_K       14
-#define KEY_L       15
-#define KEY_M       16
-#define KEY_N       17
-#define KEY_O       18
-#define KEY_P       19
-#define KEY_Q       20
-#define KEY_R       21
-#define KEY_S       22
-#define KEY_T       23
-#define KEY_U       24
-#define KEY_V       25
-#define KEY_W       26
-#define KEY_X       27
-#define KEY_Y       28
-#define KEY_Z       29
-#define KEY_1       30
-#define KEY_2       31
-#define KEY_3       32
-#define KEY_4       33
-#define KEY_5       34
-#define KEY_6       35
-#define KEY_7       36
-#define KEY_8       37
-#define KEY_9       38
-#define KEY_0       39
-
-#define KEY_F1      58
-#define KEY_F2      59
-#define KEY_F3      60
-#define KEY_F4      61
-#define KEY_F5      62
-#define KEY_F6      63
-#define KEY_F7      64
-#define KEY_F8      65
-#define KEY_F9      66
-#define KEY_F10     67
-#define KEY_F11     68
-#define KEY_F12     69
-
-#define KEY_Space   0x2c
-
-/* ------------------------------------------------------------------------- */
-
-#define NUM_KEYS    5
-
-static const uchar  keyReport[NUM_KEYS + 1][2] PROGMEM = {
-	/* none */  {0, 0},                     /* no key pressed */
-	/*  1 */    // {0, KEY_Space},
-	/*  1 */    {MOD_GUI_LEFT, KEY_L},
-	/*  2 */    {MOD_SHIFT_LEFT, KEY_2},
-	/*  3 */    {MOD_SHIFT_LEFT, KEY_3},
-	/*  4 */    {MOD_SHIFT_LEFT, KEY_4},
-};
-
-static void buildReport(uchar key)
-{
-	/* This (not so elegant) cast saves us 10 bytes of program memory */
-    *(int *)reportBuffer = pgm_read_word(keyReport[key]);
-}
 
 /* ------------------------------------------------------------------------- */
 
@@ -190,7 +98,7 @@ usbRequest_t    *rq = (void *)data;
     if((rq->bmRequestType & USBRQ_TYPE_MASK) == USBRQ_TYPE_CLASS){    /* class request type */
         if(rq->bRequest == USBRQ_HID_GET_REPORT){  /* wValue: ReportType (highbyte), ReportID (lowbyte) */
             /* we only have one report type, so don't look at wValue */
-            buildReport(keyPressed());
+            reportBuffer[0] = keyPressed();
             return sizeof(reportBuffer);
         }else if(rq->bRequest == USBRQ_HID_GET_IDLE){
             usbMsgPtr = &idleRate;
@@ -318,12 +226,8 @@ uchar   key, lastKey = 0, keyDidChange = 0;
             keyDidChange = 0;
             /* use last key and not current key status in order to avoid lost
 			 changes in key status. */
-/*
-            buildReport(lastKey);
-            usbSetInterrupt(reportBuffer, sizeof(reportBuffer));
-*/
             reportBuffer[0] = lastKey;
-            usbSetInterrupt(reportBuffer, 1);
+            usbSetInterrupt(reportBuffer, sizeof(reportBuffer));
         }
     }
     return 0;
